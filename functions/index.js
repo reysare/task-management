d75aa108221c8c11cda5dafb2fc976548afa29c0
shxxrys <reysasyahlia16@gmail.com>
2025-06-26 02:08:21 +0700

    bismillah

--- functions/index.js
+++ functions/index.js
@@ -6,18 +6,18 @@ const db = admin.firestore();
 
 exports.getTasks = functions.https.onRequest(async (req, res) => {
   const snapshot = await db.collection("tasks").get();
-  const tasks = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  const tasks = snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}));
   res.status(200).json(tasks);
 });
 
 exports.addTask = functions.https.onRequest(async (req, res) => {
   const task = req.body;
   const newTask = await db.collection("tasks").add(task);
-  res.status(201).json({ id: newTask.id, ...task });
+  res.status(201).json({id: newTask.id, ...task});
 });
 
 exports.deleteTask = functions.https.onRequest(async (req, res) => {
-  const { id } = req.query;
+  const {id} = req.query;
   await db.collection("tasks").doc(id).delete();
-  res.status(200).json({ message: "Task deleted" });
+  res.status(200).json({message: "Task deleted"});
 });
